668e93eeb59780c1c3ca205abf50dc259d0babb8
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-03-23 11:39:29 -0300

    Criando o modo escuro e claro

--- sistema-escolar/src/pages/Home.js
+++ sistema-escolar/src/pages/Home.js
@@ -1,6 +1,26 @@
+import styles from './Home.module.css';
+import { useState } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa';
+
 
 function Home(){
 
+    const[darkMode, setDarkMode] = useState(false);
+
+    return(
+        <div className={`${styles.container} ${darkMode ? styles.dark : styles.light}`}>
+            
+            <header className={styles.header}>
+                <h1>Home</h1>
+                <p>Bem Vindos a Escola React.js</p>
+
+                <button className={styles.themeBtn} onClick={() => setDarkMode(!darkMode)}>
+                {darkMode ? <FaSun/> : <FaMoon/> }
+                </button>
+        
+            </header>
+        </div>
+    );  
 }
 
 export default Home;
