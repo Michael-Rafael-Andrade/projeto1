20cdb680d63454a4061de3a6b4e644aa0cf20195
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-03-23 18:43:08 -0300

    update

--- sistema-escolar/src/pages/Home.js
+++ sistema-escolar/src/pages/Home.js
@@ -1,26 +1,61 @@
 import styles from './Home.module.css';
 import { useState } from 'react';
-import { FaSun, FaMoon } from 'react-icons/fa';
+import { FaSun, FaMoon, FaUserGraduate, FaClipboardList, FaBook } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { AiFillDashboard } from 'react-icons/ai';
 
+function Home() {
 
-function Home(){
+    const [darkMode, setDarkMode] = useState(false);
 
-    const[darkMode, setDarkMode] = useState(false);
-
-    return(
+    return (
         <div className={`${styles.container} ${darkMode ? styles.dark : styles.light}`}>
-            
+
             <header className={styles.header}>
                 <h1>Home</h1>
                 <p>Bem Vindos a Escola React.js</p>
 
                 <button className={styles.themeBtn} onClick={() => setDarkMode(!darkMode)}>
-                {darkMode ? <FaSun/> : <FaMoon/> }
+                    {darkMode ? <FaSun /> : <FaMoon />}
                 </button>
-        
+
             </header>
+
+            <section className={styles.cards}>
+                <div className={styles.card}>
+                    <FaUserGraduate className={styles.icon} />
+                    <h3>Cadastro</h3>
+                    <p>Gerenciamento de alunos</p>
+                    <Link to="/cadastrarAluno">Acessar</Link>
+                </div>
+
+                <div className={styles.card}>
+                    <FaClipboardList className={styles.icon} />
+                    <h3>Alunos</h3>
+                    <p>Alunos matriculados</p>
+                    <Link to='/alunos'>Acessar</Link>
+                </div>
+
+                <div className={styles.card}>
+                    <FaBook className={styles.icon} />
+                    <h3>Biblioteca</h3>
+                    <p>Encontre os livros em nosso acervo</p>
+                    <Link to='/biblioteca'>Acessar</Link>
+                </div>
+
+                <div className={styles.card}>
+                    <AiFillDashboard className={styles.icon} />
+                    <h3>Dashboard</h3>
+                    <p>Estatísticas da escola</p>
+                    <Link to='/dashboard'>Acessar</Link>
+                </div>
+
+            </section>
+
+
+
         </div>
-    );  
+    );
 }
 
 export default Home;
